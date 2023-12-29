2af0e53d2f352065dc4902e1e3066f382c1f7fc2
mayuresh-c19 <mayuresh1918@gmail.com>
2023-12-29 22:21:00 +0530

    Added Dist for Prod

--- reactapp/vite.config.js
+++ reactapp/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000 ,// size in KB
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom']
+        }
+      }
+    }
+  }
 })
+
+// vite.config.js
+
